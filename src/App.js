c78d42b21d499d7484296c0e5d96e35947b3394f
Yehuda1977 <42040644+Yehuda1977@users.noreply.github.com>
2021-01-11 11:12:27 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Shmuel from './pages/Shmuel'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render (){
+  
+   return (
+     
+    <Router>
+        <Container fluid={true}>
+        <Nav fill variant="tabs">
+          <Nav.Item>
+            <Nav.Link href="/Shmuel">Shmuel</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/Esther">Esther</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/Rivi">Rivi</Nav.Link>
+          </Nav.Item>
+
+        </Nav>
+        </Container>
+
+        <Route path="/Shmuel" render={() => <Shmuel/>}/>
+      </Router>  
+   )
+  
+  }
 }
 
 export default App;
+
---
+++ src/pages/Shmuel.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Shmuel(props)  {
+
+    
+    return (
+      <div style={{textAlign: 'center'}}>
+      <h1>Links</h1>
+      <a href="https://edu-il.zoom.us/j/2414969740">Ulpan with Morah Bruria</a>
+  
+      <h1>Shmuel's Schedule</h1>
+      <h2>Ulpan</h2>
+      <iframe src="https://onedrive.live.com/embed?cid=C7A2251FD4E8C014&resid=C7A2251FD4E8C014%2126371&authkey=AGQ250hN1d77lTY&em=2" width="960" height="569" frameborder="0" scrolling="no"></iframe>      
+      <h2>Morah Ilanit</h2>
+      <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vStap1Jmeh_D2vqhPF8QI0Dv-eK9NTcI3WnRyj0Sq1r0y8XX4dR-RZaMyl14RN1AMSTGt6EJTeCuOKA/embed?start=false&loop=false&delayms=3000" frameborder="0" width="960" height="569" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+      
+      </div>
+    );
+
+}
+
+export default Shmuel;
